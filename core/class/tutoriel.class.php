8858e85aa375b8405ebf37244a56e526dd08fa9b
Sylvaner <sylvain.dangin@gmail.com>
2018-02-20 23:45:22 +0100

    First commit

---
+++ core/class/tutoriel.class.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once dirname(__FILE__).'/../../../../core/php/core.inc.php';
+
+class tutoriel extends eqLogic {
+
+  /*************** Attributs ***************/
+
+  /************* Static methods ************/
+
+  /**************** Methods ****************/
+
+  /********** Getters and setters **********/
+
+}
+
+class tutorielCmd extends cmd {
+
+  /*************** Attributs ***************/
+
+  /************* Static methods ************/
+
+  /**************** Methods ****************/
+
+  public function execute($_options = array()) {
+
+  }
+
+  /********** Getters and setters **********/
+
+}
